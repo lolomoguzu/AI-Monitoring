50d182518d8d231666be1b8ebc42e25fd38f1cd6
Ron <rohaquino@gbox.ncf.edu.ph>
2025-02-27 15:15:14 +0800

    second commit

---
+++ Monitoring/src/App.tsx
@@ -0,0 +1,14 @@
+
+import HeaderTxt from "./Components/HeaderTxt";
+import NavButton from "./Components/NavButton";
+
+const App = () => {
+  return (
+  
+  <div> <HeaderTxt />
+  <NavButton/>
+  </div>
+  );
+};
+
+export default App;
---
+++ Monitoring/src/Components/HeaderTxt.tsx
@@ -0,0 +1,9 @@
+function HeaderTxt() {
+  return (
+    <div>
+      <h1>DASHBOARD</h1>
+    </div>
+  );
+}
+
+export default HeaderTxt;
